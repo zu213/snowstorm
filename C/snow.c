892f185d625a26bc16a37c5760ee874fcfd149b3
Zach Upstone <zu213@bath.ac.uk>
2025-01-19 09:20:32 +0000

    initial snow for C

--- C/snow.c
+++ C/snow.c
@@ -1,57 +1,85 @@
 #include <windows.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 //gcc file.c -o executable
 
-void paint_snow(){
+#define WIDTH 320
+#define HEIGHT 240
+const int length = WIDTH * HEIGHT;
+DWORD pixels[WIDTH * HEIGHT] = {0};
+HDC hdc;
+HDC hdcMem;
+HBITMAP hBitmap;
+BITMAPINFO bitmapInfo;
+int random;
 
+void setupNewSnow(){
+    random = rand() % WIDTH;
+    pixels[random] = RGB(255, 255, 255);
+
+}
+
+void animateSnow(){
+    // SLEEP - this will change speed snow falls
+    Sleep(100);
+    // You can also modify pixels here
+    int pixelIndex = HEIGHT * WIDTH - 1;
+    setupNewSnow();
+
+    for(int i = HEIGHT; i > 0; i--){
+        for(int j = 0; j < WIDTH; j++){
+            if(GetRValue(pixels[pixelIndex]) == 255){
+                pixels[pixelIndex] = RGB(0, 0, 0);
+                if(pixelIndex + WIDTH < length){
+                    pixels[pixelIndex + WIDTH] = RGB(255, 255, 255);
+                }
+            }
+            pixelIndex--;
+        }
+    }
+    //printf("%i", GetRValue(pixels[pixelIndex]));
+    //printf("%i", GetGValue(pixels[pixelIndex]));
+    //printf("%i", GetBValue(pixels[pixelIndex]));
+}
+
+
+void setupScreen(HWND hwnd, WPARAM wParam, LPARAM lParam){
+    hdc = GetDC(hwnd);
+    hdcMem = CreateCompatibleDC(hdc);
+
+    // Create bitmap and put it into memory
+    hBitmap = CreateCompatibleBitmap(hdc, WIDTH, HEIGHT);
+    SelectObject(hdcMem, hBitmap);
+
+    // Set important parts of the bitmap
+    bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bitmapInfo.bmiHeader.biWidth = WIDTH;
+    bitmapInfo.bmiHeader.biHeight = -HEIGHT;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 32;
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    // load pixels from bitmap
+    
+    GetDIBits(hdcMem, hBitmap, 0, HEIGHT, pixels, &bitmapInfo, DIB_RGB_COLORS);
+
+}
+
+void editScreen(HWND hwnd, WPARAM wParam, LPARAM lParam){
+
+    animateSnow(pixels, WIDTH, HEIGHT);
+
+    // Update the bitmap with modified pixel data
+    SetDIBits(hdcMem, hBitmap, 0, HEIGHT, pixels, &bitmapInfo, DIB_RGB_COLORS);
+    BitBlt(hdc, 0, 0, WIDTH, HEIGHT, hdcMem, 0, 0, SRCCOPY);
 }
 
 // Windows procedure 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_PAINT: {
-            // Setup the window context
-            HDC hdc = GetDC(hwnd);
-            HDC hdcMem = CreateCompatibleDC(hdc);
-            int width = 320, height = 240;
-
-            // Create bitmap and put it into memory
-            HBITMAP hBitmap = CreateCompatibleBitmap(hdc, width, height);
-            SelectObject(hdcMem, hBitmap);
-
-            // Drawing on bit map (set colour then draw)
-            SetBkColor(hdcMem, RGB(255, 255, 255));
-            SetTextColor(hdcMem, RGB(0, 0, 0));
-
-            SetPixel(hdcMem, 100, 100, RGB(255, 0, 0));
-
-
-            // Set important parts of the bitmap
-            BITMAPINFO bitmapInfo;
-            bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-            bitmapInfo.bmiHeader.biWidth = width;
-            bitmapInfo.bmiHeader.biHeight = -height;
-            bitmapInfo.bmiHeader.biPlanes = 1;
-            bitmapInfo.bmiHeader.biBitCount = 32;
-            bitmapInfo.bmiHeader.biCompression = BI_RGB;
-
-            // load pixels from bitmap
-            DWORD *pixels = (DWORD *)malloc(width * height * sizeof(DWORD));
-            GetDIBits(hdcMem, hBitmap, 0, height, pixels, &bitmapInfo, DIB_RGB_COLORS);
-
-            // You can also modify pixels here
-            int pixelIndex = (50 * width) + 50;
-            pixels[pixelIndex] = RGB(0, 0, 255);
-
-            // Update the bitmap with modified pixel data
-            SetDIBits(hdcMem, hBitmap, 0, height, pixels, &bitmapInfo, DIB_RGB_COLORS);
-            BitBlt(hdc, 0, 0, width, height, hdcMem, 0, 0, SRCCOPY);
-
-            // Cleanup
-            free(pixels);
-            DeleteObject(hBitmap);
-            DeleteDC(hdcMem);
-            ReleaseDC(hwnd, hdc);
+            editScreen(hwnd, wParam, lParam);
             break;
         }
 
@@ -66,6 +94,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 }
 
 int main() {
+    // initialise random
+    srand(time(NULL));   // Initialization, should only be called once.
+
     // Setup window details
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WindowProc;
@@ -74,9 +105,12 @@ int main() {
     RegisterClass(&wc); // Let windows create the window
 
     // Create window
-    HWND hwnd = CreateWindowEx(0, wc.lpszClassName, "Pixel Manipulation in Windows",
+    HWND hwnd = CreateWindowEx(0, wc.lpszClassName, "Snow",
                                WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 320, 240,
                                NULL, NULL, wc.hInstance, NULL);
+
+    // setup snow
+    setupScreen(hwnd, WIDTH, HEIGHT);
 
     // Main message loop - Windows work this way you need to keep doing this to update the graphics
     MSG msg;
@@ -84,6 +118,11 @@ int main() {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    // Cleanup
+    DeleteObject(hBitmap);
+    DeleteDC(hdcMem);
+    ReleaseDC(hwnd, hdc);
 
     return 0;
 }
